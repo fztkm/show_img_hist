b9994df6efb7e21bd7aeeb8b62a9d9ce62484bed
Takumi Fukuzawa <t.fukuzawa.986@nitech.jp>
2024-05-31 16:58:27 +0900

    fix

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ def get_image():
     """download the image
     """
 
-    img_url = \
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
+    img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
 
     im = imread(img_url)
 
